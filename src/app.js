12c28c21b7b31d776d6fb895ca53475c4e95d82d
vlauncher <v2dent@gmail.com>
2024-06-18 15:19:32 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const doteenv = require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server runiing on ${PORT}`);
+});
